abe9f07a98f6dbdafb0c946bd548579b74981bef
x-jirox <correalenin045gmail.com>
2025-04-28 20:59:23 -0500

    add messages in server

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatMessage } from 'src/app/models/chat-message';
+import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent {
+export class ChatComponent  implements OnInit{
+  
+  constructor(private chatService: ChatService) { }
+
+  ngOnInit(): void {
+    this.chatService.joinRoom('ABC');
+  }
+
+  sendMessage() {
+    // Logic to send message
+    const chatMessage = {
+      message: 'Hello',
+      user: '1'
+    }as ChatMessage  
+    this.chatService.sendMessage('ABC', chatMessage);
+  }
 
 }
